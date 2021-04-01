0b49d7606521d4eb507babaa5fdc1e0f949d9111
taweewat-tk <taweewat.tkz@gmail.com>
2021-04-02 03:19:10 +0700

    remove comment code

--- src/content/sectionContent/component/SubContentMain.js
+++ src/content/sectionContent/component/SubContentMain.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { Col, Row } from "react-bootstrap";
 import styled from "styled-components";
 import device from "../../../devices/device";
 
--- src/devices/device.js
+++ src/devices/device.js
@@ -1,18 +1,3 @@
-// const size = {
-//     mobileS: '320px',
-//     mobileM: '375px',
-//     mobileL: '425px',
-//     tablet: '768px',
-//     laptop: '1024px',
-
-
-//     laptopL: '1440px',
-
-//     mobile: '767px',
-//     tablet: '768px',
-//     desktop: '992px',
-// }
-
 export const device = {
     mobile: `(max-width: 768px)`,
     tablet: `(min-width: 768px) and (max-width: 1199px)`,
